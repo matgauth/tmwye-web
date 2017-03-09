da9bed70df0fdaf14ed585da613fbd1d5893a83f
mathieugauthier <mathieu.gauthier.isen@gmail.com>
2017-03-09 22:35:42 +0100

    fixed and inverted navbar

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -3,35 +3,28 @@ import { Link } from "react-router-dom";
 import { auth } from "../../config/constants";
 import { Menu, Button, Icon } from "semantic-ui-react";
 
-const Navigation = ({ loggedIn, signOut }) => {
-  if (loggedIn && auth.currentUser) {
-    return (
-      <Menu>
-        <Menu.Menu position="right">
+const Navigation = ({ loggedIn, signOut }) => (
+  <Menu inverted fixed="top">
+    {loggedIn && auth.currentUser
+      ? <Menu.Menu position="right">
           <Menu.Item>
             <Link to="/account">
-              <Icon name="user" size="large" />{auth.currentUser.email}
+              <Icon name="user" />{auth.currentUser.email}
             </Link>
           </Menu.Item>
           <Menu.Item>
-            <Button primary icon="sign out" onClick={signOut}></Button>
+            <Button secondary icon="sign out" onClick={signOut} />
           </Menu.Item>
         </Menu.Menu>
-      </Menu>
-    );
-  }
-  return (
-    <Menu>
-      <Menu.Menu position="right">
-        <Menu.Item>
-          <Link to="/login"><Icon size="large" name="sign in"/></Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link to="/register"><Icon size="large" name="signup"/></Link>
-        </Menu.Item>
-      </Menu.Menu>
-    </Menu>
-  );
-};
+      : <Menu.Menu position="right">
+          <Menu.Item>
+            <Link to="/login"><Icon name="sign in" /></Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="/register"><Icon name="signup" /></Link>
+          </Menu.Item>
+        </Menu.Menu>}
+  </Menu>
+);
 
 export default Navigation;
